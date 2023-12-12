61b506096f4f876a468bbfd20674b7811943c00d
sosheo <jordanrobins@hotmail.co.uk>
2023-12-12 21:05:43 +0000

    feat: add placeholder keyup handler

--- components/text-input/EmojiInput.tsx
+++ components/text-input/EmojiInput.tsx
@@ -3,13 +3,21 @@ import { forwardRef } from "react";
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const EmojiInput = forwardRef<HTMLInputElement, InputProps>(
-    ({ className, ...props }, ref) => (
-        <input
+    ({ className, ...props }, ref) => {
+
+        function handleKeyup(event: any) {
+            if ((event.keyCode === 186) || (event.key === ":")) {
+                console.log("show menu");
+            }
+        }
+
+        return (<input
             type="text"
             ref={ref}
             {...props}
-        />
-    ),
+            onKeyUp={handleKeyup}
+        />);
+    }
 );
 EmojiInput.displayName = "EmojiInput";
 
