914e2ed11e54a025e2e0ec71981504921ce634ba
sosheo <jordanrobins@hotmail.co.uk>
2023-12-15 22:03:13 +0000

    feat: use emoji menu

--- components/text-input/EmojiInput.tsx
+++ components/text-input/EmojiInput.tsx
@@ -1,22 +1,33 @@
-import { forwardRef } from "react";
+import { forwardRef, useState } from "react";
+import { EmojiMenu } from "./EmojiMenu";
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const EmojiInput = forwardRef<HTMLInputElement, InputProps>(
     ({ className, ...props }, ref) => {
+        const [showMenu, setShowMenu] = useState(false);
 
         function handleKeyup(event: any) {
             if ((event.keyCode === 186) || (event.key === ":")) {
-                console.log("show menu");
+                setShowMenu(true);
+            }
+
+            if ((event.keyCode === 27) || event.key === "escape") {
+                setShowMenu(false);
             }
         }
 
-        return (<input
-            type="text"
-            ref={ref}
-            {...props}
-            onKeyUp={handleKeyup}
-        />);
+        return (
+            <>
+                <input
+                    type="text"
+                    ref={ref}
+                    {...props}
+                    onKeyUp={handleKeyup}
+                />
+                {showMenu && <EmojiMenu />  }
+            </>
+        );
     }
 );
 EmojiInput.displayName = "EmojiInput";
