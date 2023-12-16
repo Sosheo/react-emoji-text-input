c4df3fa794890ba32ce314021e5805fb61531031
sosheo <jordanrobins@hotmail.co.uk>
2023-12-16 18:53:35 +0000

    feat: update example

--- example/src/components/text-input/EmojiInput.jsx
+++ example/src/components/text-input/EmojiInput.jsx
@@ -1,34 +1,22 @@
-import { forwardRef, useState, createRef, useRef } from "react";
+import { forwardRef, useState } from "react";
 import { EmojiMenu } from "./EmojiMenu";
 const EmojiInput = forwardRef(({ className, ...props }, ref) => {
     const [showMenu, setShowMenu] = useState(false);
-    const ref2 = useRef(null);
-
     function handleKeyup(event) {
         if ((event.keyCode === 186) || (event.key === ":")) {
             setShowMenu(true);
-                    
-            const node = ref2.current;
-            const size = {
-                width: node.offsetWidth,
-                height: node.offsetHeight
-            };
-            const position = {
-                x: node.offsetLeft,
-                y: node.offsetTop
-            };
-            console.log(size, position);
         }
         if ((event.keyCode === 27) || event.key === "escape") {
             setShowMenu(false);
         }
     }
-    return (
-        <div ref={ref}>
-            <input type="text" ref={ref2} {...props} onKeyUp={handleKeyup}/>
-            {showMenu && <EmojiMenu />}
-        </div>
-    );
+    function closeMenu() {
+        setShowMenu(false);
+    }
+    return (<div>
+                <input type="text" ref={ref} {...props} onKeyUp={handleKeyup}/>
+                {showMenu && <EmojiMenu closeMenu={closeMenu}/>}
+            </div>);
 });
 EmojiInput.displayName = "EmojiInput";
 export { EmojiInput };
--- example/src/components/text-input/EmojiMenu.jsx
+++ example/src/components/text-input/EmojiMenu.jsx
@@ -1,5 +1,18 @@
+import { useRef, useEffect } from "react";
 import emojis from "./emojis.json";
-const EmojiMenu = () => {
+const EmojiMenu = ({ closeMenu }) => {
+    const ref = useRef(null);
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (ref.current && !ref.current.contains(event.target)) {
+                closeMenu();
+            }
+        }
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [ref]);
     const menuStyle = {
         position: "absolute",
         padding: "10px",
@@ -11,9 +24,32 @@ const EmojiMenu = () => {
         height: "280px",
         overflowX: "hidden",
         overflowY: "scroll",
+        scrollbarWidth: "thin",
     };
     return (<>
-            <div style={menuStyle}>
+            {/* @ts-ignore */}
+            <style jsx>{`
+                .emoji-menu {
+                    scrollbar-width: thin;
+                    scrollbar-color: blue orange;
+                }
+
+                .emoji-menu::-webkit-scrollbar {
+                    width: 10px;
+                }
+                
+                .emoji-menu::-webkit-scrollbar-track {
+                    background: transparent;
+                }
+                
+                .emoji-menu::-webkit-scrollbar-thumb {
+                    background-color: black;
+                    border-radius: 20px;
+                    border: 3px solid grey;
+                }
+            `}</style>
+
+            <div ref={ref} className={'emoji-menu'} style={menuStyle}>
                 {emojis.map((emoji, index) => (<span key={index} style={{ flexBasis: "16.6%" }}>
                         {emoji.e}
                     </span>))}
