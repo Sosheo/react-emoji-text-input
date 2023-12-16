b6c99328ab22b60febfd95b6fda3e9603672409f
sosheo <jordanrobins@hotmail.co.uk>
2023-12-16 16:10:01 +0000

    chore: update example

--- example/src/components/text-input/EmojiMenu.jsx
+++ example/src/components/text-input/EmojiMenu.jsx
@@ -1,12 +1,25 @@
+import emojis from "./emojis.json";
 const EmojiMenu = () => {
     const menuStyle = {
         position: "absolute",
         padding: "10px",
         border: "1px solid white",
         backgroundColor: "gray",
+        display: "flex",
+        width: "280px",
+        flexWrap: "wrap",
+        height: "280px",
+        overflowX: "hidden",
+        overflowY: "scroll",
     };
     return (<>
-            <div style={menuStyle}>Menu</div>
+            <div style={menuStyle}>
+                {emojis.map((emoji, index) => (<span key={index} style={{ flexBasis: "16.6%" }}>
+                        {emoji.e}
+                    </span>))}
+
+
+            </div>
         </>);
 };
 export { EmojiMenu };
