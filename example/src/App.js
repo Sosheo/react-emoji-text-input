c2953a6797dbe42af39660c8af3b32f37cd5e27d
sosheo <jordanrobins@hotmail.co.uk>
2023-12-17 19:39:49 +0000

    feat: add styles to examples

--- example/src/App.js
+++ example/src/App.js
@@ -8,9 +8,17 @@ function App() {
         <h1>Emoji Text Input</h1>
         <p>A simple text input and textarea react component with built in emoji menu.</p>
 
-        <EmojiInput type="textinput" placeholder="Enter text here, type ':' to open the emoji menu" />
+        <EmojiInput
+            type="textinput"
+            style={{width: "300px", fontSize: "1.1rem", padding: "10px"}}
+            placeholder="Enter text here, type ':' to open the emoji menu"
+        />
 
-        <EmojiInput type="textarea" placeholder="Enter text here, type ':' to open the emoji menu" />
+        <EmojiInput
+            type="textarea"
+            style={{width: "300px", height: "200px", fontSize: "1.1rem", padding: "10px", marginTop: "20px"}}
+            placeholder="Enter text here, type ':' to open the emoji menu"
+        />
 
         {/* <EmojiTextarea placeholder="Enter text here, type ':' to open the emoji menu" /> */}
 
